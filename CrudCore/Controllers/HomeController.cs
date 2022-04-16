adfe34436043c8015398cebb0d152d8280188394
Manoj Kumar Yadav <manukr2020@gmail.com>
2022-04-16 16:53:19 +0530

    first commit

---
+++ CrudCore/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using CrudCore.Db_Context;
+using CrudCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        studentlistContext obj = new studentlistContext();
+        public IActionResult Index()
+        {
+            var res = obj.Students.ToList();
+            List<StudentMod> sobj = new List<StudentMod>();
+            foreach (var item in res)
+            {
+                sobj.Add(new StudentMod 
+                {
+                   Id=item.Id, 
+                   Name=item.Name, 
+                   Email=item.Email, 
+                   Password=item.Password 
+
+                });
+            }
+            return View(sobj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var del = obj.Students.Where(m => m.Id == id).First();
+            obj.Students.Remove(del);
+            obj.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(StudentMod sobj)
+        {
+            Student stobj = new Student();
+            stobj.Id = sobj.Id;
+            stobj.Name = sobj.Name;
+            stobj.Email = sobj.Email;
+            stobj.Password = sobj.Password;
+            if (sobj.Id == 0)
+            { 
+            obj.Students.Add(stobj);
+            obj.SaveChanges();
+            }
+            else
+            {
+                obj.Entry(stobj).State = EntityState.Modified;
+                obj.SaveChanges();
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            StudentMod st = new StudentMod();
+            var edititem = obj.Students.Where(m => m.Id == id).First();
+            st.Id = edititem.Id;
+            st.Name = edititem.Name;
+            st.Email = edititem.Email;
+            st.Password = edititem.Password;
+            ViewBag.id = edititem.Id;
+            return View("AddStudent",st);
+        }
+
+        public IActionResult Privacy()
+        {
+            HttpContext.Session.SetString("name","manoj kumar");
+            var session = HttpContext.Session.GetString("name");
+            //ViewBag.Session = HttpContext.Session.GetString("name");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudCore/Db_Context/studentlistContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using CrudCore.Models;
+
+#nullable disable
+
+namespace CrudCore.Db_Context
+{
+    public partial class studentlistContext : DbContext
+    {
+        public studentlistContext()
+        {
+        }
+
+        public studentlistContext(DbContextOptions<studentlistContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KQ1JKKM;Database=studentlist;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("students");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<CrudCore.Models.StudentMod> StudentMod { get; set; }
+    }
+}
